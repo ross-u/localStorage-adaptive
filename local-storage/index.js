0dd4d37c12ebe76d5560fb0d4099e8cfe8e42a42
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-11-04 09:48:08 +0100

    Initial commit

---
+++ local-storage/index.js
@@ -0,0 +1,28 @@
+// Create an array that contains objects that you want to save in the localStorage
+
+/* 
+  name: 'Anna', score: 4120,
+  name: 'Bob', score: 1245,
+  name: 'Sarah', score: 5699,
+  name: 'Marc', score: 3455
+
+*/
+
+// Stringify the data before storing in localStorage
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+
+// Save the data to the localStorage
+// https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
+
+// Retrieve the stored data from local storage
+//  https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
+
+// Parse the data back into an object/array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+
+// Insert the new score into the array retrieved from the localStorage
+//  name: 'Emma', score: 6701
+
+// Stringify the updated array and save it back in the localStorage
+
+// retreive the updated array from the local storage, to check if the previous step was done right
